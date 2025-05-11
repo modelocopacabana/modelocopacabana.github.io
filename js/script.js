6f2637ab9eee2ef0c75b55eed689970439b4e112
terracriptografia <terracriptografia@gmail.com>
2025-05-11 11:03:15 -0300

    inicio

---
+++ js/script.js
@@ -0,0 +1,276 @@
+	$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll <= 2851) { 
+    $("#whatsapp").fadeOut();
+  } else {
+    $("#whatsapp").fadeIn();
+  }
+});
+	$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll <= 2851) { 
+    $("#pedido_finalizado").fadeOut();
+  } else {
+    $("#pedido_finalizado").fadeIn();
+  }
+});
+
+  $(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 2850) { 
+    $("#icone_inf").fadeOut();
+  } else {
+    $("#icone_inf").fadeIn();
+  }
+});
+
+function 
+leiaMais_b() {
+  var pontos_b=document.getElementById("pontos_b");
+  var maisTexto_b=document.getElementById("maiss_b");
+  var btnLeiaMais_b=document.getElementById("btnLeiaMais_b");
+
+  if(pontos_b.style.display ==="none"){
+    pontos_b.style.display="inline";
+    maisTexto_b.style.display="none";
+    btnLeiaMais_b.innerHTML="Abrir";
+  }else{
+      pontos_b.style.display="none";
+    maisTexto_b.style.display="inline";
+    btnLeiaMais_b.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_1() {
+  var pontos_1=document.getElementById("pontos_1");
+  var maisTexto_1=document.getElementById("maiss_1");
+  var btnLeiaMais_1=document.getElementById("btnLeiaMais_1");
+
+  if(pontos_1.style.display ==="none"){
+    pontos_1.style.display="inline";
+    maisTexto_1.style.display="none";
+    btnLeiaMais_1.innerHTML="Ler";
+  }else{
+      pontos_1.style.display="none";
+    maisTexto_1.style.display="inline";
+    btnLeiaMais_1.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_2() {
+  var pontos_2=document.getElementById("pontos_2");
+  var maisTexto_2=document.getElementById("maiss_2");
+  var btnLeiaMais_2=document.getElementById("btnLeiaMais_2");
+
+  if(pontos_2.style.display ==="none"){
+    pontos_2.style.display="inline";
+    maisTexto_2.style.display="none";
+    btnLeiaMais_2.innerHTML="Ler";
+  }else{
+      pontos_2.style.display="none";
+    maisTexto_2.style.display="inline";
+    btnLeiaMais_2.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_3() {
+  var pontos_3=document.getElementById("pontos_3");
+  var maisTexto_3=document.getElementById("maiss_3");
+  var btnLeiaMais_3=document.getElementById("btnLeiaMais_3");
+
+  if(pontos_3.style.display ==="none"){
+    pontos_3.style.display="inline";
+    maisTexto_3.style.display="none";
+    btnLeiaMais_3.innerHTML="Ler";
+  }else{
+      pontos_3.style.display="none";
+    maisTexto_3.style.display="inline";
+    btnLeiaMais_3.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_4() {
+  var pontos_4=document.getElementById("pontos_4");
+  var maisTexto_4=document.getElementById("maiss_4");
+  var btnLeiaMais_4=document.getElementById("btnLeiaMais_4");
+
+  if(pontos_4.style.display ==="none"){
+    pontos_4.style.display="inline";
+    maisTexto_4.style.display="none";
+    btnLeiaMais_4.innerHTML="Ler";
+  }else{
+      pontos_4.style.display="none";
+    maisTexto_4.style.display="inline";
+    btnLeiaMais_4.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_5() {
+  var pontos_5=document.getElementById("pontos_5");
+  var maisTexto_5=document.getElementById("maiss_5");
+  var btnLeiaMais_5=document.getElementById("btnLeiaMais_5");
+
+  if(pontos_5.style.display ==="none"){
+    pontos_5.style.display="inline";
+    maisTexto_5.style.display="none";
+    btnLeiaMais_5.innerHTML="Abrir";
+  }else{
+      pontos_5.style.display="none";
+    maisTexto_5.style.display="inline";
+    btnLeiaMais_5.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_6() {
+  var pontos_6=document.getElementById("pontos_6");
+  var maisTexto_6=document.getElementById("maiss_6");
+  var btnLeiaMais_6=document.getElementById("btnLeiaMais_6");
+
+  if(pontos_6.style.display ==="none"){
+    pontos_6.style.display="inline";
+    maisTexto_6.style.display="none";
+    btnLeiaMais_6.innerHTML="Abrir";
+  }else{
+      pontos_6.style.display="none";
+    maisTexto_6.style.display="inline";
+    btnLeiaMais_6.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_7() {
+  var pontos_7=document.getElementById("pontos_7");
+  var maisTexto_7=document.getElementById("maiss_7");
+  var btnLeiaMais_7=document.getElementById("btnLeiaMais_7");
+
+  if(pontos_7.style.display ==="none"){
+    pontos_7.style.display="inline";
+    maisTexto_7.style.display="none";
+    btnLeiaMais_7.innerHTML="Ver";
+  }else{
+      pontos_7.style.display="none";
+    maisTexto_7.style.display="inline";
+    btnLeiaMais_7.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_8() {
+  var pontos_8=document.getElementById("pontos_8");
+  var maisTexto_8=document.getElementById("maiss_8");
+  var btnLeiaMais_8=document.getElementById("btnLeiaMais_8");
+
+  if(pontos_8.style.display ==="none"){
+    pontos_8.style.display="inline";
+    maisTexto_8.style.display="none";
+    btnLeiaMais_8.innerHTML="Leia Mais";
+  }else{
+      pontos_8.style.display="none";
+    maisTexto_8.style.display="inline";
+    btnLeiaMais_8.innerHTML="Fechar";
+  }
+}
+function 
+leiaMais_9() {
+  var pontos_9=document.getElementById("pontos_9");
+  var maisTexto_9=document.getElementById("maiss_9");
+  var btnLeiaMais_9=document.getElementById("btnLeiaMais_9");
+
+  if(pontos_9.style.display ==="none"){
+    pontos_9.style.display="inline";
+    maisTexto_9.style.display="none";
+    btnLeiaMais_9.innerHTML="Leia Mais";
+  }else{
+      pontos_9.style.display="none";
+    maisTexto_9.style.display="inline";
+    btnLeiaMais_9.innerHTML="Fechar";
+  }
+}
+function 
+leiaMais_10() {
+  var pontos_10=document.getElementById("pontos_10");
+  var maisTexto_10=document.getElementById("maiss_10");
+  var btnLeiaMais_10=document.getElementById("btnLeiaMais_10");
+
+  if(pontos_10.style.display ==="none"){
+    pontos_10.style.display="inline";
+    maisTexto_10.style.display="none";
+    btnLeiaMais_10.innerHTML="Leia Mais";
+  }else{
+      pontos_10.style.display="none";
+    maisTexto_10.style.display="inline";
+    btnLeiaMais_10.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_11() {
+  var pontos_11=document.getElementById("pontos_11");
+  var maisTexto_11=document.getElementById("maiss_11");
+  var btnLeiaMais_11=document.getElementById("btnLeiaMais_11");
+
+  if(pontos_11.style.display ==="none"){
+    pontos_11.style.display="inline";
+    maisTexto_11.style.display="none";
+    btnLeiaMais_11.innerHTML="Leia Mais";
+  }else{
+      pontos_11.style.display="none";
+    maisTexto_11.style.display="inline";
+    btnLeiaMais_11.innerHTML="Fechar";
+  }
+}
+
+function 
+leiaMais_12() {
+  var pontos_12=document.getElementById("pontos_12");
+  var maisTexto_12=document.getElementById("maiss_12");
+  var btnLeiaMais_12=document.getElementById("btnLeiaMais_12");
+
+  if(pontos_12.style.display ==="none"){
+    pontos_12.style.display="inline";
+    maisTexto_12.style.display="none";
+    btnLeiaMais_12.innerHTML="Leia Mais";
+  }else{
+      pontos_12.style.display="none";
+    maisTexto_12.style.display="inline";
+    btnLeiaMais_12.innerHTML="Fechar";
+  }
+}
+function 
+leiaMais_13() {
+  var pontos_13=document.getElementById("pontos_13");
+  var maisTexto_13=document.getElementById("maiss_13");
+  var btnLeiaMais_13=document.getElementById("btnLeiaMais_13");
+
+  if(pontos_13.style.display ==="none"){
+    pontos_13.style.display="inline";
+    maisTexto_13.style.display="none";
+    btnLeiaMais_13.innerHTML="Leia Mais";
+  }else{
+      pontos_13.style.display="none";
+    maisTexto_13.style.display="inline";
+    btnLeiaMais_13.innerHTML="Fechar";
+  }
+}
+function 
+leiaMais_14() {
+  var pontos_14=document.getElementById("pontos_14");
+  var maisTexto_14=document.getElementById("maiss_14");
+  var btnLeiaMais_14=document.getElementById("btnLeiaMais_14");
+
+  if(pontos_14.style.display ==="none"){
+    pontos_14.style.display="inline";
+    maisTexto_14.style.display="none";
+    btnLeiaMais_14.innerHTML="Leia Mais";
+  }else{
+      pontos_14.style.display="none";
+    maisTexto_14.style.display="inline";
+    btnLeiaMais_14.innerHTML="Fechar";
+  }
+}
